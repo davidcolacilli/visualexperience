7e0114c911ddd7b4f066e176af85d5a9401ae9c9
“davidcolacilli” <davidcolacilli@users.noreply.github.com>
2016-09-27 16:55:58 -0300

    live version

--- web/moma/html5up-hyperspace/index.php
+++ web/moma/html5up-hyperspace/index.php
@@ -1,13 +1,26 @@
-<!DOCTYPE HTML>
-<!--
-	Hyperspace by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
+<?php
+if(isset($_POST['submit'])){
+    // $to = "monica@moma-translations.com, magdalena@moma-translations.com";
+	$to = "davidcolacilli@gmail.com";
+    $from = $_POST['email'];
+    $name = $_POST['name'];
+    $subject = "Contact form submission";
+    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+	
+    $headers = "From:" . $from;
+    mail($to,$subject,$message,$headers);
+	echo "<aside>";
+	echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+	echo "</aside>";
+	// You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
+
+<!DOCTYPE html>
 <html>
 
 <head>
-    <title>Moma: Focused on your business</title>
+    <title>Moma Translations</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
@@ -21,7 +34,9 @@
     <!-- Sidebar -->
     <section id="sidebar">
         <div class="inner">
-            <img src="images/moma-logo.svg" alt="Moma Translations">
+			<div class="logo">
+				<img src="images/moma.jpg" alt="Moma Translations">
+			</div>
             <nav>
                 <ul>
                     <li><a href="#one">About us</a></li>
@@ -35,85 +50,39 @@
     <!-- Wrapper -->
     <div id="wrapper">
 
-        <!-- Intro -->
-        <!--<section id="intro" class="wrapper style1 fullscreen fade-up">
-                <div class="inner">
-                    <h1>Some cool heading here</h1>
-                    <p>And some other awesome and catchy text to put as a warm welcome for your clients.</p>
-                    <p>This was released under the <a href="http://html5up.net/license">Creative Commons</a>.</p>
-                    <ul class="actions">
-                        <li><a href="#one" class="button scrolly">Learn more</a></li>
-                    </ul>
-                </div>
-            </section>-->
-
-        <!-- Two -->
+        <!-- One -->
         <section id="one" class="wrapper style1 fade-up">
             <div class="inner">
                 <h1>Focused on your business</h1>
                 <h2>About us</h2>
                 <ul>
-                    <li>Literary, Technical & Scientific Certified Senior English/Spanish Translators</li>
+                    <li>Literary, Technical &amp; Scientific Certified Senior English&frasl;Spanish Translators</li>
                     <li>Working professionally in the Translation Industry since 2003</li>
                     <li>Trusted by many Translation firms from around the world</li>
                     <li>International Memberships</li>
                 </ul>
-                <!--<div class="features">
-								<section>
-									<span class="icon major fa-code"></span>
-									<h3>Medicina</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
-								</section>
-								<section>
-									<span class="icon major fa-lock"></span>
-									<h3>Farmacia</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
-								</section>
-								<section>
-									<span class="icon major fa-cog"></span>
-									<h3>Salud</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
-								</section>
-								<section>
-									<span class="icon major fa-desktop"></span>
-									<h3>Negocios / Marketing</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
-								</section>
-								<section>
-									<span class="icon major fa-chain"></span>
-									<h3>Tecnología de la información</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
-								</section>
-								<section>
-									<span class="icon major fa-diamond"></span>
-									<h3>Técnica</h3>
-									<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>
-								</section>
-							</div>-->
                 <ul class="actions">
                     <li><a href="#two" class="button scrolly">Learn more</a></li>
                 </ul>
             </div>
         </section>
 
-        <!-- One -->
+        <!-- Two -->
         <section id="two" class="wrapper style2 spotlights">
             <section>
-                <!--<a href="#" class="image"><img src="images/pic01.jpg" alt="" data-position="center center" /></a>-->
                 <div class="content">
                     <div class="inner">
                         <h2>Magdalena Gastaldi</h2>
                         <p>Literary, Technical and Scientific English-Spanish Translator (degree dated December 2003, granted by Instituto Superior No 28 “Olga Cossettini”, Rosario, Santa Fe, Argentina, <a href="http://www.iesoc.com.ar" target="_blank">www.iesoc.com.ar</a>)</p>
                         <p>Licensed by Colegio de Traductores; Province of Santa Fe (<a href="http://www.traductoresrosario.org.ar" target="_blank">www.traductoresrosario.org.ar</a>)</p>
                         <ul class="actions">
-                            <li><a href="https://ar.linkedin.com/in/magdalena-gastaldi-486a9421" class="button" target="_blank">Linked In</a></li>
+                            <li><a href="https://ar.linkedin.com/in/magdalena-gastaldi-486a9421" class="button" target="_blank">LinkedIn</a></li>
                             <li><a href="http://www.proz.com/profile/82652" class="button" target="_blank">Proz.com</a></li>
                         </ul>
                     </div>
                 </div>
             </section>
             <section>
-                <!--<a href="#" class="image"><img src="images/pic02.jpg" alt="" data-position="top center" /></a>-->
                 <div class="content">
                     <div class="inner">
                         <h2>Mónica Belén Colacilli</h2>
@@ -121,7 +90,7 @@
                         <p>Post Graduate Studies in English-Spanish Translation (National University of Rosario, <a href="http://www.fhumyar.unr.edu.ar" target="_blank">www.fhumyar.unr.edu.ar</a>)</p>
                         <p>Licensed by Colegio de Traductores; Province of Santa Fe (<a href="http://www.traductoresrosario.org.ar" target="_blank">www.traductoresrosario.org.ar</a>)</p>
                         <ul class="actions">
-                            <li><a href="https://ar.linkedin.com/in/mónica-belén-colacilli-95ab8222" class="button" target="_blank">Linked In</a></li>
+                            <li><a href="https://ar.linkedin.com/in/mónica-belén-colacilli-95ab8222" class="button" target="_blank">LinkedIn</a></li>
                             <li><a href="http://www.proz.com/profile/54283" class="button" target="_blank">Proz.com</a></li>
                             <li><a href="https://www.facebook.com/profile.php?id=100010611916035" class="button" target="_blank">Facebook</a></li>
                         </ul>
@@ -130,29 +99,26 @@
             </section>
         </section>
 
-
         <!-- Three -->
         <section id="three" class="wrapper style1 fade-up">
             <div class="inner">
                 <h2>Get in touch</h2>
                 <div class="split style1">
                     <section>
-                        <form method="post" action="#">
+                        <form  action="index.php" method="POST" enctype="multipart/form-data" id="contact-form">
                             <div class="field half first">
                                 <label for="name">Name</label>
-                                <input type="text" name="name" id="name" />
+                                <input name="name" id="name" type="text" value="" size="30" required/>
                             </div>
                             <div class="field half">
                                 <label for="email">Email</label>
-                                <input type="text" name="email" id="email" />
+                                <input name="email" id="email" type="email" value="" size="30" required/>
                             </div>
                             <div class="field">
                                 <label for="message">Message</label>
-                                <textarea name="message" id="message" rows="5"></textarea>
+                                <textarea name="message" id="message" rows="5" cols="30" required></textarea>
                             </div>
-                            <ul class="actions">
-                                <li><a href="" class="button submit">Send Message</a></li>
-                            </ul>
+                            <input type="submit" name="submit" value="Send Message"/>
                         </form>
                     </section>
                     <section>
@@ -180,15 +146,9 @@
     </div>
 
     <footer>
-        <a href="#one" class="scrolly"><img src="images/moma-logo.svg" alt="Moma Translations"></a>
+        <a href="#one" class="scrolly"><img src="images/moma-dark.jpg" alt="Moma Translations"></a>
+        <div></div>
     </footer>
-    <!--<footer id="footer" class="wrapper style1-alt">
-        <div class="inner">
-            <ul class="menu">
-                <li>2016 - Moma Translations</li>
-            </ul>
-        </div>
-    </footer>-->
 
     <!-- Scripts -->
     <script src="assets/js/jquery.min.js"></script>
@@ -198,7 +158,6 @@
     <script src="assets/js/util.js"></script>
     <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
     <script src="assets/js/main.js"></script>
-
 </body>
 
 </html>
